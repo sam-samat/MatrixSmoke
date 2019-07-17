ba1301b017f5ecc9a8d66d865947bead71f7c308
samat <bluewalnut996@gmail.com>
2019-07-17 01:21:35 -0500

    Test1 and Test2 by Samat are done

--- src/test/java/matrixSmoke/MatrixSmokeSuite.java
+++ src/test/java/matrixSmoke/MatrixSmokeSuite.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.Driver;
 import utilities.TestBase;
 
@@ -23,7 +24,29 @@ public class MatrixSmokeSuite extends TestBase {
 
         WebElement createLeaveRequestMenu = Driver.getDriver().findElement(By.xpath("//div[@class='o_form_sheet_bg']"));
 
-        Assert.assertTrue(createLeaveRequestMenu.isDisplayed(),"Create leave menu is not displayed");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(createLeaveRequestMenu.isDisplayed(),"Create leave menu is not displayed");
+
+        softAssert.assertAll();
 
     }
+
+    @Test
+    public void checkIfCalendarIsDisplayed(){
+        WebElement leavesButton = Driver.getDriver().findElement(By.linkText("Leaves"));
+        leavesButton.click();
+
+        WebElement leaveRequestButton = Driver.getDriver().findElement(By.linkText
+                ("Leaves Request"));
+        leaveRequestButton.click();
+
+        WebElement calendar = Driver.getDriver().findElement(By.xpath("//div[@class='o_main_content']"));
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(calendar.isDisplayed(),"Calendar is not displayed");
+
+        softAssert.assertAll();
+    }
+
+
 }
